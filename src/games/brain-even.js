1da0163b88d81b739365f772d4a81c7cc28d656f
Martishevich Oleg <oleg.martishevich@gmail.com>
2018-10-24 10:43:44 +0300

    fix remarks one more time

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,27 +2,25 @@ import readlineSync from 'readline-sync';
 import { greeting, askName } from '../helpers';
 
 const successNumber = 3;
-const checkIsEven = number => number % 2 === 0;
+const isEven = number => number % 2 === 0;
 const getRandomNumber = () => Math.floor(Math.random() * 100) + 1;
-const getRightAnswer = number => (checkIsEven(number) ? 'yes' : 'no');
+const getRightAnswer = number => (isEven(number) ? 'yes' : 'no');
 
 export default () => {
   greeting();
   console.log('Answer "yes" if number even otherwise answer "no".');
   const name = askName();
-  let guessedNumber = 0;
-  for (guessedNumber = 0; guessedNumber < successNumber; guessedNumber += 1) {
+  for (let i = 0; i < successNumber; i += 1) {
     const question = getRandomNumber();
     const rightAnswer = getRightAnswer(question);
     console.log(`Question: ${question}`);
     const answer = readlineSync.question('Your answer: ');
     if (answer !== rightAnswer) {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${rightAnswer}'`);
-      break;
+      return 0;
     }
     console.log('Correct!');
   }
-  if (guessedNumber === successNumber) {
-    console.log(`Congratulations, ${name}`);
-  }
+  console.log(`Congratulations, ${name}`);
+  return 0;
 };
